5b19669e0d978f3b503bd196cb7a77e4a0912b8a
IsabellaSoares <isabella.soares.28@gmail.com>
2021-10-05 14:02:05 -0300

    feat: config Firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,19 +1,19 @@
 // NOTE: import only the Firebase modules that you need in your app... except
 // for the second line, which makes both the linter and react-firebase happy
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 // Initalize Firebase.
 var firebaseConfig = {
-  apiKey: "AIzaSyBOKD-RgLT5A1xaPQCwtvxD32auxgU1hNg",
-  authDomain: "tcl-34-smart-shopping-list.firebaseapp.com",
-  projectId: "tcl-34-smart-shopping-list",
-  storageBucket: "tcl-34-smart-shopping-list.appspot.com",
-  messagingSenderId: "364163197766",
-  appId: "1:364163197766:web:45b462aa078b06cd5c5347"
+  apiKey: 'AIzaSyBOKD-RgLT5A1xaPQCwtvxD32auxgU1hNg',
+  authDomain: 'tcl-34-smart-shopping-list.firebaseapp.com',
+  projectId: 'tcl-34-smart-shopping-list',
+  storageBucket: 'tcl-34-smart-shopping-list.appspot.com',
+  messagingSenderId: '364163197766',
+  appId: '1:364163197766:web:45b462aa078b06cd5c5347',
 };
 
-const firebaseInstance = firebase.initializeApp(firebaseConfig);
-const db = firebaseInstance.firestore();
+const firebaseInstance = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseInstance);
 
 export { db };
