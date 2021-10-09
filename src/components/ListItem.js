626e09db0318237a68f4e0fcfa8cff02ac634a4e
IsabellaSoares <isabella.soares.28@gmail.com>
2021-10-09 11:13:51 -0300

    fix: fixes data display logic

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,14 +3,23 @@ import { useProducts } from '../hooks/useProducts';
 const ListItem = () => {
   const { products, loading, error } = useProducts();
 
+  const showProducts = () => {
+    if (products.length === 0) {
+      return <p>You don't have any product in your list</p>;
+    }
+
+    return products.map(({ id, name, date }) => (
+      <li key={id}>{`Product: ${name} => Created at: ${new Date(
+        date,
+      ).toLocaleString()}`}</li>
+    ));
+  };
+
   return (
     <div>
-      {loading && 'Loading'}
-      {error && 'Error'}
-      {products &&
-        products.map(({ id, name, date }) => (
-          <li key={id}>{`Product: ${name} => Created at: ${date}`}</li>
-        ))}
+      {loading ? <p>Loading...</p> : null}
+      {error ? <p>There was an error</p> : null}
+      {!loading ? showProducts() : null}
     </div>
   );
 };
