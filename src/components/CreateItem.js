808a2a8c6bb68c231d8eb24138738257b0adfa64
IsabellaSoares <isabella.soares.28@gmail.com>
2021-10-09 11:41:24 -0300

    feat: use new addProduct function

--- src/components/CreateItem.js
+++ src/components/CreateItem.js
@@ -1,13 +1,26 @@
-import { useProducts } from '../hooks/useProducts';
+import { useState } from 'react';
+import { addProduct } from '../utils/firebaseUtils';
 
 const CreateItem = () => {
-  const { addProduct } = useProducts();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  const handleClick = async () => {
-    await addProduct();
+  const handleClick = () => {
+    setIsLoading(true);
+
+    addProduct()
+      .then((res) => console.log(res))
+      .catch((error) => setError(error))
+      .finally(() => setIsLoading(false));
   };
 
-  return <button onClick={handleClick}>Add new product</button>;
+  return (
+    <div>
+      <button onClick={handleClick}>Add new product</button>
+      {isLoading ? <p>Adding product...</p> : null}
+      {error ? <p>Unable to add product. Please, try again.</p> : null}
+    </div>
+  );
 };
 
 export default CreateItem;
