490f9b2432e2d6947104fcaa0ce49664daeb7ecd
Yancarlo Urdaneta <yancarlou@gmail.com>
2021-10-11 11:09:17 -0400

    comments on useLocalStorage usage

--- src/components/CreateList/index.js
+++ src/components/CreateList/index.js
@@ -1,5 +1,19 @@
 import { getToken } from '@the-collab-lab/shopping-list-utils';
+import { useLocalStorage } from '../../hooks/useLocalStorage.js';
 
 export const CreateList = () => {
+  /*
+  As our custom hook useLocalStorage receives 2 params
+  itemName and initialValue we must pass them when using the hook
+
+  itemName is how we are calling that portion of local storage in use
+  initialValue is what data type we are going to store
+
+  */
+
+  const LOCAL_STORAGE = 'ITEM_LIST_V1';
+  const [storedValue, setValue] = useLocalStorage(LOCAL_STORAGE, []);
+
+  setValue(getToken());
   return <>{console.log(getToken())}</>;
 };
