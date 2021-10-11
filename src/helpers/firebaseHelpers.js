a5cc8e5d7a63e85652251135543ee9962dccfb0b
IsabellaSoares <isabella.soares.28@gmail.com>
2021-10-11 17:04:15 -0300

    feat: add new fields

--- src/helpers/firebaseHelpers.js
+++ src/helpers/firebaseHelpers.js
@@ -1,5 +1,8 @@
 export const parseData = (querySnapshot) =>
   querySnapshot.docs.reduce((acc, doc) => {
-    const { name, date } = doc.data();
-    return [...acc, { id: doc.id, name, date }];
+    const { name, user_token, deadline, last_purchase_date } = doc.data();
+    return [
+      ...acc,
+      { id: doc.id, name, user_token, deadline, last_purchase_date },
+    ];
   }, []);
