e1984effa974177ebd80c2529c003a933ac44f3e
IsabellaSoares <isabella.soares.28@gmail.com>
2021-10-11 16:06:36 -0300

    feat: add new styles

--- src/pages/AddItemPage/index.js
+++ src/pages/AddItemPage/index.js
@@ -3,8 +3,8 @@ import CreateItem from '../../components/CreateItem';
 import Navigation from '../../components/routing/Navigation';
 
 export const AddItemPage = () => (
-  <div className="container">
-    <h1>Add Item</h1>
+  <div className="content-container">
+    <h1 className="fixed title">Add Item</h1>
     <CreateItem />
     <Navigation />
   </div>
--- src/pages/ListPage/index.js
+++ src/pages/ListPage/index.js
@@ -3,8 +3,8 @@ import ListItem from '../../components/ListItem';
 import Navigation from '../../components/routing/Navigation';
 
 export const ListPage = () => (
-  <div className="container">
-    <h1>List</h1>
+  <div className="content-container">
+    <h1 className="fixed title">List</h1>
     <ListItem />
     <Navigation />
   </div>
