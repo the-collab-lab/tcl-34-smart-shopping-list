9dfc6d294f15a5f0c8c0f33cce4b1e434a71d7c9
Yancarlo Urdaneta <yancarlou@gmail.com>
2021-10-13 12:33:03 -0400

    missing closing curly bracket

--- src/components/CreateList/index.js
+++ src/components/CreateList/index.js
@@ -1,12 +1,13 @@
 import { getToken } from '@the-collab-lab/shopping-list-utils';
 import { useLocalStorage } from '../../hooks/useLocalStorage';
+
 export const CreateList = () => {
   const LOCAL_STORAGE = 'ITEM_LIST_V1';
   const [storedValue, setValue] = useLocalStorage(LOCAL_STORAGE, '');
+
   const generateToken = () => {
     const token = getToken();
-    console.log(token);
-    setValue(getToken());
+    setValue(token);
   };
   return <button onClick={() => generateToken()}>create</button>;
 };
