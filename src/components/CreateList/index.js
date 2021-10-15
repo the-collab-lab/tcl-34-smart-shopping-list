0bc7455a66399ae1fd3b811e8e68eb15790e2c01
Yancarlo Urdaneta <yancarlou@gmail.com>
2021-10-15 14:08:23 -0400

    Moving 3rd party libraries to the top and destructuring useHistory method

--- src/components/CreateList/index.js
+++ src/components/CreateList/index.js
@@ -1,13 +1,12 @@
 import { getToken } from '@the-collab-lab/shopping-list-utils';
-import { useLocalStorage } from '../../hooks/useLocalStorage';
 import { useHistory } from 'react-router-dom';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
 
 export const CreateList = () => {
-  let history = useHistory();
-  const push = history.push;
+  const { push } = useHistory();
 
   const LOCAL_STORAGE = 'ITEM_LIST_V1';
-  const [storedValue, setValue] = useLocalStorage(LOCAL_STORAGE, '');
+  const [setValue] = useLocalStorage(LOCAL_STORAGE, '');
 
   const generateToken = () => {
     const token = getToken();
