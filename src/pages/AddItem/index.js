51c82c828f3503b19a4a3103491e72b4e4e2bca4
Yancarlo Urdaneta <yancarlou@gmail.com>
2021-10-19 17:09:28 -0400

    searching if item already exists

--- src/pages/AddItem/index.js
+++ src/pages/AddItem/index.js
@@ -5,9 +5,14 @@ import ContentContainer from '../../components/content-container';
 import AddForm from '../../components/add-form';
 import Navigation from '../../components/routing/Navigation';
 import { addProduct } from '../../utils/firebaseUtils';
+
+//Hooks
+import { useProducts } from '../../hooks/useProducts';
+
 import './styles.css';
 
 export const AddItemPage = () => {
+  const { products } = useProducts();
   const [isLoading, setIsLoading] = useState(false);
   const [message, setMessage] = useState(null);
   const [error, setError] = useState(false);
@@ -21,26 +26,47 @@ export const AddItemPage = () => {
     event.preventDefault();
     setIsLoading(true);
 
-    addProduct(formState)
-      .then((res) => console.log(res))
-      .catch(() => setError(true))
-      .finally(() => {
-        setIsLoading(false);
+    /*
+    Checks the state if the product already exists
+    Find returns undefined if it has not matches
+    */
+    const checkProduct = products.find(
+      (product) => product.productName === formState.productName,
+    );
 
-        setFormState({
-          productName: '',
-          timeFrame: '7',
-          lastPurchaseDate: null,
+    /*    const regExp = '/[.,-_{}+!¡°()`~¨]/g'
+    console.log("1 "+checkProduct)
+    const data  = checkProduct.productName
+    const normalizeData = data.replace(regExp, "")
+    console.log("2" + normalizeData)*/
+    /*
+    We have to compare explicitely if it is undefined, otherwise
+    it wouldn't work as expected
+    */
+    if (checkProduct === undefined) {
+      addProduct(formState)
+        .then((res) => console.log(res))
+        .catch(() => setError(true))
+        .finally(() => {
+          setIsLoading(false);
+          console.log(formState.productName);
+          setFormState({
+            productName: '',
+            timeFrame: '7',
+            lastPurchaseDate: null,
+          });
         });
+    } else {
+      setIsLoading(false);
+      setError(true);
+    }
+    setMessage(
+      error
+        ? 'It seems you have already added this product. Try another one.'
+        : 'Successfully created product!',
+    );
 
-        setMessage(
-          error
-            ? 'Unable to add product. Please, try again.'
-            : 'Successfully created product!',
-        );
-
-        setTimeout(() => setMessage(null), 3000);
-      });
+    setTimeout(() => setMessage(null), 3000);
   };
 
   const handleForm = (event) => {
