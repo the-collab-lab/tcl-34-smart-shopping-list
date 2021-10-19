fb21e6731aa0d707dba0751c9f7a23811471696d
unknown <mailliwmogollon@gmail.com>
2021-10-19 16:08:07 -0400

    add JoinList

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { CreateList } from '../../components/CreateList';
+import { JoinList } from '../../components/JoinList';
 
 export const Welcome = () => (
   <div className="container">
@@ -8,5 +9,9 @@ export const Welcome = () => (
       Smart Shopping list!
     </h1>
     <CreateList />
+    <p>Join an existing shopping list<br/>
+    by entering a three word token.
+    </p>
+    <JoinList />
   </div>
 );
