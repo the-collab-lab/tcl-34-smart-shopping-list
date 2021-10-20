bb7601e9738f6c53faece7d2632677f6c311749e
IsabellaSoares <isabella.soares.28@gmail.com>
2021-10-20 19:26:14 -0300

    refactor: handle list errors on listing page

--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -1,15 +1,42 @@
 import React from 'react';
-import ItemsList from '../../components/items-list';
+import { useHistory } from 'react-router-dom';
 import Title from '../../components/title';
 import ContentContainer from '../../components/content-container';
 import Navigation from '../../components/routing/Navigation';
+import Button from '../../components/button';
+import { useList } from '../../hooks/useList';
 
-export const ListPage = () => (
-  <>
-    <Title>List</Title>
-    <ContentContainer>
-      <ItemsList />
-    </ContentContainer>
-    <Navigation />
-  </>
-);
+export const ListPage = () => {
+  const { list, loading, error } = useList();
+  const { push } = useHistory();
+
+  const showProducts = () => {
+    if (list.length === 0) {
+      return <p>You don't have any product in your list </p>;
+    }
+
+    return (
+      <ul>
+        {list.map(({ date, productName }) => (
+          <li key={date}>{productName}</li>
+        ))}
+      </ul>
+    );
+  };
+
+  return (
+    <>
+      <Title>List</Title>
+      <ContentContainer>
+        {loading ? <p>Loading...</p> : null}
+        {!loading && error ? (
+          <Button onClick={() => push('/')}>
+            There was an error, try again
+          </Button>
+        ) : null}
+        {!loading && !error ? showProducts() : null}
+      </ContentContainer>
+      {!loading && !error ? <Navigation /> : null}
+    </>
+  );
+};
