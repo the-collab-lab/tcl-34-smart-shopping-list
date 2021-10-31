9189f4f0c6ea09486cd37897d433fecd2d2ccd30
IsabellaSoares <isabella.soares.28@gmail.com>
2021-10-31 12:48:23 -0300

    feat: update showProducts with checkboxes

--- src/components/show-products/index.js
+++ src/components/show-products/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { useHistory } from 'react-router';
 import Button from '../button';
+import { updatePurchaseDate } from '../../utils/firebaseUtils';
 import { useProducts } from '../../hooks/useProducts';
+import {
+  useLocalStorage,
+  LOCAL_STORAGE_LIST_TOKEN,
+} from '../../hooks/useLocalStorage';
+import { TimeFrames } from '../../utils/timeFrames';
+import { compareDates } from '../../utils/compareDates';
 import ContentContainer from '../content-container';
 import './styles.css';
 
 export const ShowProducts = () => {
   const { products, loading } = useProducts();
   const { push } = useHistory();
+  const { storedValue } = useLocalStorage(LOCAL_STORAGE_LIST_TOKEN);
+  const timeFrames = TimeFrames;
+
+  const handleOnChange = (productID) => {
+    updatePurchaseDate(productID, storedValue);
+  };
 
   if (loading) {
     return (
@@ -31,8 +44,25 @@ export const ShowProducts = () => {
   return (
     <ContentContainer>
       <ul>
-        {products.map(({ id, productName }) => (
-          <li key={id}>{productName}</li>
+        {products?.map(({ id, productName, lastPurchaseDate, timeFrame }) => (
+          <li className="checkbox-item" key={id}>
+            <label htmlFor={id} className="checkbox-label">
+              <input
+                type="checkbox"
+                id={id}
+                name={productName}
+                onChange={() => handleOnChange(id)}
+                checked={
+                  lastPurchaseDate && compareDates(lastPurchaseDate.toDate())
+                }
+                ariaLabel={timeFrames[timeFrame]}
+              />
+              <span
+                className={`checkmark checkbox-timeFrame-${timeFrame}`}
+              ></span>
+              <span className="checkbox-name">{productName}</span>
+            </label>
+          </li>
         ))}
       </ul>
     </ContentContainer>
