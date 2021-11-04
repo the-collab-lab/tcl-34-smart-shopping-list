1d9e2b922d0a5590c342b2ee1a26223a40e7fca4
Yancarlo Urdaneta <yancarlou@gmail.com>
2021-11-03 22:34:06 -0400

    add number of purchases and days until next purchase

--- src/helpers/firebaseHelpers.js
+++ src/helpers/firebaseHelpers.js
@@ -2,9 +2,25 @@
 
 export const parseData = (querySnapshot) =>
   querySnapshot.docs.reduce((acc, doc) => {
-    const { productName, listToken, timeFrame, lastPurchaseDate } = doc.data();
+    const {
+      productName,
+      listToken,
+      timeFrame,
+      lastPurchaseDate,
+      daysUntilNextPurchase,
+      numberOfPurchases,
+    } = doc.data();
+
     return [
       ...acc,
-      { id: doc.id, productName, listToken, timeFrame, lastPurchaseDate },
+      {
+        id: doc.id,
+        productName,
+        listToken,
+        timeFrame,
+        lastPurchaseDate,
+        daysUntilNextPurchase,
+        numberOfPurchases,
+      },
     ];
   }, []);
