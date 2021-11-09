09dc7403f09d943ee9d9cbe8b4f817d010cc87b3
Yancarlo Urdaneta <yancarlou@gmail.com>
2021-11-09 17:43:26 -0400

    time frames for aria labels

--- src/utils/timeFrames.js
+++ src/utils/timeFrames.js
@@ -3,3 +3,10 @@ export const TimeFrames = {
   14: 'Kind of soon',
   30: 'Not soon',
 };
+
+export const TimeFrameLabels = {
+  inactive: 'This item is inactive due to the amount of time',
+  soon: 'You are expected to buy this item soon',
+  kindOfSoon: 'You are expected to buy this item kind of soon',
+  notAnySoon: 'You are not expected to buy this item any time soon',
+};
