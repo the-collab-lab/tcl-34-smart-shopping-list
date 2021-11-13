66e8edad3712030c615d58a25bcda1f43118ec97
Yancarlo Urdaneta <yancarlou@gmail.com>
2021-11-13 19:27:17 -0400

    remove sort function

--- src/helpers/firebaseHelpers.js
+++ src/helpers/firebaseHelpers.js
@@ -1,12 +1,11 @@
 // In this file we add the utility functions that we can reuse in other parts of the code (data parse, filters) that are related to using Firebase
 import { nextPurchaseDay } from '../utils/nextPurchaseDay';
-import { SortProductsByTime } from '../utils/sortProductsByTime';
 
 export const parseData = (querySnapshot) =>
   querySnapshot.docs.reduce((acc, doc) => {
     const { id } = doc;
     const product = doc.data();
-    return SortProductsByTime([
+    return [
       ...acc,
       {
         id,
@@ -21,5 +20,5 @@ export const parseData = (querySnapshot) =>
           product.numberOfPurchases,
         ),
       },
-    ]);
+    ];
   }, []);
