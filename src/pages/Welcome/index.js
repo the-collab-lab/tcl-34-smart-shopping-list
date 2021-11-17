2c7b43ff34717b94aaac9b8d7490e410fd6f19ac
susana <susir95@gmail.com>
2021-11-17 17:27:50 -0500

    delete content-container from welcome view, adding styles for PC view

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import { CreateList } from '../../components/create-list';
 import { JoinList } from '../../components/join-list';
-import ContentContainer from '../../components/content-container';
 import Header from '../../components/title';
 
 import './style.css';
 
 export const Welcome = () => (
-  <ContentContainer>
-    <main className="welcome-container">
-      <Header className="welcome-header">
-        Welcome to your Smart Shopping List!
-      </Header>
-      <div className="welcome-form">
-        <CreateList />
-        <p className="welcome-text center">- or -</p>
-        <p className="welcome-text">
-          Join an existing shopping list by entering a three word token.
-        </p>
-        <JoinList />
-      </div>
-    </main>
-  </ContentContainer>
+  <main className="welcome-container">
+    <Header className="welcome-header">
+      Welcome to your Smart Shopping List!
+    </Header>
+    <div className="welcome-form">
+      <CreateList />
+      <p className="welcome-text center">- or -</p>
+      <p className="welcome-text">
+        Join an existing shopping list by entering a three word token.
+      </p>
+      <JoinList />
+    </div>
+  </main>
 );
