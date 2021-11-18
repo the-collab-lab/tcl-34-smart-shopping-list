0221d7c9aa639e4d2a650156efc7d76b8c52037d
IsabellaSoares <isabella.soares.28@gmail.com>
2021-11-18 11:09:13 -0300

    refactor: apply new pages layout

--- src/pages/AddItem/index.js
+++ src/pages/AddItem/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
 import Button from '../../components/button';
-import Header from '../../components/title';
 import ContentContainer from '../../components/content-container';
 import AddForm from '../../components/add-form';
 import Navigation from '../../components/routing/Navigation';
@@ -80,20 +79,17 @@ export const AddItemPage = () => {
   };
 
   return (
-    <>
-      <Header className="page-header">Add Item</Header>
-      <ContentContainer>
-        <form onSubmit={onSubmit} className="add-form">
-          <AddForm handleForm={handleForm} formState={formState} />
-          <Button type="submit" disabled={formState.productName === ''}>
-            Add Item
-          </Button>
-        </form>
+    <ContentContainer>
+      <form onSubmit={onSubmit} className="add-form">
+        <AddForm handleForm={handleForm} formState={formState} />
+        <Button type="submit" disabled={formState.productName === ''}>
+          Add Item
+        </Button>
+      </form>
 
-        {isLoading ? <p>Adding product...</p> : null}
-        {message ? <p>{message}</p> : null}
-      </ContentContainer>
+      {isLoading ? <p>Adding product...</p> : null}
+      {message ? <p>{message}</p> : null}
       <Navigation />
-    </>
+    </ContentContainer>
   );
 };
--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import Header from '../../components/title';
 import Navigation from '../../components/routing/Navigation';
 import { ShowProducts } from '../../components/show-products';
 
+import '../../components/show-products/styles.css';
+import ContentContainer from '../../components/content-container';
+
 export const ListPage = () => {
   return (
-    <>
-      <Header className="page-header">List</Header>
+    <ContentContainer>
       <ShowProducts />
       <Navigation />
-    </>
+    </ContentContainer>
   );
 };
