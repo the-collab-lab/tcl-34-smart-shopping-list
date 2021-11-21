6910ce2375f3eea7e07afb717b4c53c65b508200
Susana Ruiz <susir95@gmail.com>
2021-11-20 22:55:53 -0500

    Welcome view (#33)
    * adding styles to welcome view and svg image
    
    * delete content-container from welcome view, adding styles for PC view
    
    * fixing alphabetical order in css styles and styles in App.css
    
    * fixing background image as img and adding styles
    
    * adding styles to errorAuth
    
    * fixing semantic problems and styles for the views
    
    * fixing semantic tag in welcome view

--- src/components/join-list/index.js
+++ src/components/join-list/index.js
@@ -38,7 +38,7 @@ export const JoinList = () => {
 
   return (
     <form onSubmit={retrieveList} className="add-form">
-      <label htmlFor="productName">
+      <label className="share-token" htmlFor="productName">
         Share token
         <input
           className="input-item"
--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { CreateList } from '../../components/create-list';
 import { JoinList } from '../../components/join-list';
-import ContentContainer from '../../components/content-container';
 import Header from '../../components/title';
+import shoppingWelcomeImage from './shopping-welcome-image.svg';
 
 import './style.css';
 
 export const Welcome = () => (
-  <ContentContainer>
+  <main className="welcome-container">
     <Header className="welcome-header">
-      Welcome to your <br />
-      Smart Shopping list!
+      Welcome to your Smart Shopping List!
     </Header>
-    <div className="welcome-form">
-      <CreateList />
-      <p className="welcome-text center">- or -</p>
-      <p className="welcome-text">
-        Join an existing shopping list
-        <br />
-        by entering a three word token.
-      </p>
-      <JoinList />
-    </div>
-  </ContentContainer>
+    <section className="welcome-content">
+      <img
+        className="shopping-cart-image"
+        src={shoppingWelcomeImage}
+        alt="person shopping with a shopping cart"
+      />
+      <div className="welcome-form">
+        <CreateList />
+        <p className="welcome-text center">- or -</p>
+        <p className="welcome-text">
+          Join an existing shopping list by entering a three word token.
+        </p>
+        <JoinList />
+      </div>
+    </section>
+  </main>
 );
