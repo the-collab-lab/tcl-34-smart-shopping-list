1fc76f65f3f765a272362c99873463c1f9021083
Yancarlo Urdaneta <yancarlou@gmail.com>
2021-11-27 21:28:45 -0400

    fix criteria for items with zero purchases

--- src/utils/nextPurchaseDay.js
+++ src/utils/nextPurchaseDay.js
@@ -1,4 +1,6 @@
 import { diffBetweenTodayAndDate } from './diffBetweenTodayAndDate';
+import { TimeFrames } from './timeFrames';
+import { getKeyByValue } from './getKeyByValue';
 /***
  * First we verify by specificity and
  * we return the string that is used for the
@@ -9,6 +11,7 @@ export const nextPurchaseDay = (
   daysUntilNextPurchase,
   lastPurchaseDate,
   numberOfPurchases,
+  timeFrame,
 ) => {
   /**
    * After some checks, we noticed lastPurchasedDate is a Date object
@@ -28,7 +31,11 @@ export const nextPurchaseDay = (
   const nextEstimatedPurchase = 2 * daysUntilNextPurchase;
 
   if (numberOfPurchases === 0) {
-    return 'soon';
+    /*
+    This either returns a string with soon, kindOfSoon or notAnySoon
+    that'll depend on the timeframe that comes from Firestore
+    */
+    return getKeyByValue(TimeFrames, timeFrame);
   }
   if (
     numberOfPurchases === 1 ||
